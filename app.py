2a9a546df4666dc5c0424fe9f9bd4bb3653618de
viktorlov <orlovva@ymail.com>
2023-04-26 12:01:14 +0300

    (l) code review

--- app.py
+++ app.py
@@ -1,1 +1,19 @@
-...
+number = 101
+
+def is_prime(num):
+    flag = True
+    for i in range(2, num):
+        if num % i == 0:
+            flag = False
+            break
+    if num != 1 and flag == True:
+        print('Число', num, 'простое.')
+    else:
+        print('Число', num, 'составное.')
+
+
+x = 17
+y = int(input())
+is_prime(x)
+is_prime(y)
+is_prime(number)
