1b8a6cadbe048a46029b4f0509e230cf6620fcf6
viktorlov <orlovva@ymail.com>
2023-04-27 11:34:10 +0300

    (l) code review

--- app.py
+++ app.py
@@ -1,19 +1,10 @@
-number = 101
+x = 5
 
-def is_prime(num):
-    flag = True
-    for i in range(2, num):
-        if num % i == 0:
-            flag = False
-            break
-    if num != 1 and flag == True:
-        print('Число', num, 'простое.')
-    else:
-        print('Число', num, 'составное.')
+def add():
+    global x
+    x = 3
+    x = x + 5
+    print(x)
 
-
-x = 17
-y = int(input())
-is_prime(x)
-is_prime(y)
-is_prime(number)
+add()
+print(x)
